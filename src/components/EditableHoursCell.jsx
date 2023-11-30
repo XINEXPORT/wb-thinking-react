0bac14f0d817564ab61d2e615e5eb4f57e524cd3
Christine Hoang <christinehoangt@gmail.com>
2023-11-30 13:46:22 -0600

    initsave

---
+++ src/components/EditableHoursCell.jsx
@@ -0,0 +1,16 @@
+const EditableHoursCell =({value,isEditing, onValueChange})=>{
+    return isEditing ?(
+        <td>
+            <input
+            type="text"
+            value={value}
+            onChange={(e)=> onValueChange(e.target.value)}/>
+        </td>
+    ) : (
+        <td>
+            {value}
+        </td>
+    )
+}
+
+export default EditableHoursCell
--- src/components/InvoiceTable.jsx
+++ src/components/InvoiceTable.jsx
@@ -1,1 +1,60 @@
 import './InvoiceTable.css';
+import InvoiceTableAddButton from './InvoiceTableAddButton';
+import InvoiceTableHeader from './InvoiceTableHeader';
+import InvoiceTableRow from './InvoiceTableRow';
+import generateId from '../utils/idGenerator.js'
+import { useState } from 'react';
+
+const InvoiceTable =({initialInvoiceList})=>{
+const [invoiceList, setInvoiceList]=useState(initialInvoiceList);
+
+const addInvoiceRow=()=>{
+ const newInvoicelist=[...invoiceList]
+ newInvoicelist.push({
+    id: generateId(),
+    description: 'description',
+    rate: '',
+    hours: '',
+    isEditing: true
+ })
+ setInvoiceList(newInvoicelist)
+}
+
+const deleteInvoiceRow=(id)=>{
+    const newInvoiceList=invoiceList.filter((invoice)=>invoice.id !==id)
+    setInvoiceList(newInvoiceList)
+}
+
+    const rows=invoiceList.map((invoiceItem)=>{
+        const {id, description, rate, hours,isEditing} = invoiceItem
+    return(
+        <InvoiceTableRow
+        key={id}
+        initialInvoiceData={{description, rate, hours}}
+        initialIsEditing={isEditing}
+        onDeleteRow={()=> deleteInvoiceRow(id)}
+        />
+    )
+}
+)
+
+    return(
+        <table>
+            <thead>
+                <InvoiceTableHeader/>
+            </thead>
+            <tbody>
+               {rows}
+            </tbody>
+            <tfoot>
+                <InvoiceTableAddButton addRow={addInvoiceRow}/>
+            </tfoot>
+        </table>
+    )
+}
+
+export default InvoiceTable
+
+
+
+
